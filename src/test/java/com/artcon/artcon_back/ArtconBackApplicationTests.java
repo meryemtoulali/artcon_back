4cc02ddb047ed84502bde47dd02da383954bf892
TheSilentDame02 <larroussi.sara@gmail.com>
2023-11-23 22:33:34 +0000

    spring boot app generated - db configuration working

---
+++ src/test/java/com/artcon/artcon_back/ArtconBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artcon.artcon_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtconBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
