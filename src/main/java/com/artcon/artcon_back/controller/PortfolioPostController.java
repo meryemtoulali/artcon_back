95993314c43a884aee562f8151e6f28f7b0b167b
khadijalaatitine <khadijalaatitine01@gmail.com>
2024-01-16 07:15:18 +0100

    update portfolio like response

--- src/main/java/com/artcon/artcon_back/controller/PortfolioPostController.java
+++ src/main/java/com/artcon/artcon_back/controller/PortfolioPostController.java
@@ -1,6 +1,7 @@
 package com.artcon.artcon_back.controller;
 
 import com.artcon.artcon_back.model.LikeRequest;
+import com.artcon.artcon_back.model.LikeResponse;
 import com.artcon.artcon_back.service.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,18 +20,24 @@ public class PortfolioPostController {
     UserService userService;
 
     @PostMapping("/portfolioPostLike")
-    public ResponseEntity<?> likePortfolioPost(@RequestBody LikeRequest request) {
+    public LikeResponse likePortfolioPost(@RequestBody LikeRequest request) {
         Integer userId = request.getUser_id();
         Integer postId = request.getPost_id();
         likeService.likePortfolioPost(userId, postId);
-        return ResponseEntity.ok("PortfolioPost liked successfully");
+        LikeResponse res = new LikeResponse();
+        res.setSuccess(true);
+        res.setMessage("Portfolio Post liked successfully");
+        return res;
     }
     @PostMapping("/portfolioPostDislike")
-    public ResponseEntity<?> dislikePortfolioPost( @RequestBody LikeRequest request) {
+    public LikeResponse dislikePortfolioPost( @RequestBody LikeRequest request) {
         Integer userId = request.getUser_id();
         Integer postId = request.getPost_id();
         likeService.dislikePortfolioPost(userId, postId);
-        return ResponseEntity.ok("Portfolio Post disliked successfully");
+        LikeResponse res = new LikeResponse();
+        res.setSuccess(true);
+        res.setMessage("Portfolio Post disliked successfully");
+        return res;
     }
 
     @GetMapping("/hasUserLikedPortfolioPost")
